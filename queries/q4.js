8436413f9fafd963b403814114d1dad070e4bafa
Samuel Roberson <73807913+swroberson@users.noreply.github.com>
2021-12-06 13:36:53 -0600

    Create q4.js

---
+++ queries/q4.js
@@ -0,0 +1,13 @@
+let q4 = function(zip, from, to) {
+    return `select BLDG_ACTYRBLT as Year, median(LAND_SQ_FT) as Median_Lot_Size from
+    BUILDING natural join PARCEL
+    where BLDG_ACTYRBLT >= ${from} and BLDG_ACTYRBLT <= ${to}
+    and SITE_ZIP = ${zip}
+    and BLDG_ACTYRBLT is not null
+    group by BLDG_ACTYRBLT
+    order by BLDG_ACTYRBLT`
+}
+
+module.exports = {
+    get: q4
+}
