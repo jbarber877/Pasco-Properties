e6e85e20bd88c34d16ee1b245e87b06447a81602
Timothy Schneider <timothycschneider@outlook.com>
2021-12-06 16:39:25 -0500

    added query 1 functionality

--- server.js
+++ server.js
@@ -6,7 +6,89 @@ const { outFormat } = require('oracledb');
 
 const oracledb = require('oracledb');
 
+async function db_query1(zip, from, to) {
+  let connection;
+  let chart_file_text;
+  try {
 
+    connection = await oracledb.getConnection({ user: "Barber.J", password: "RedCedar3", connectionString: "oracle.cise.ufl.edu/orcl" });
+
+    console.log("Successfully connected to Oracle Database");
+    let text = `select BLDG_ACTYRBLT as Year, median(LAND_SQ_FT) as Median_Lot_Size from
+    BUILDING natural join PARCEL
+    where BLDG_ACTYRBLT >= ${from} and BLDG_ACTYRBLT <= ${to}
+    and SITE_ZIP = ${zip}
+    and BLDG_ACTYRBLT is not null
+    group by BLDG_ACTYRBLT
+    order by BLDG_ACTYRBLT`;
+
+    let result = await connection.execute(text);
+      let years = [];
+      let percentages =[];
+      result.rows.forEach((pair) => {
+        years.push(pair[0]);
+        percentages.push(pair[1]);
+      })
+      //console.log(years);
+      //console.log(percentages);
+      // pass info from database to chart
+      chart_file_text = `<!DOCTYPE html>
+      <html lang="en">
+          <head>
+          </head>
+          <body>
+              <h1>Query Results</h1>
+              <h2 id="return">return</h2>
+              <div >
+                  <canvas id="myChart"></canvas>
+              </div>
+              <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+              <script >
+                  document.getElementById("return").onclick = () => {
+                      location.search = '';
+                  }
+                  const labels = [` + years + `];
+          const data = {
+            labels: labels,
+            datasets: [{
+              label: 'Median lot size in zip code ` + zip + ` between ` + from + ` and ` + to + `',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [` + percentages + `]
+          }]
+          };
+          const config = {
+            type: 'line',
+            data: data,
+            options: {}
+          };
+            // === include 'setup' then 'config' above ===
+          
+            const myChart = new Chart(
+              document.getElementById('myChart'),
+              config
+            );
+              </script>
+          </body>
+      </html>` 
+      fs.writeFile('Query1_chart.html', chart_file_text, err => {
+          if (err){
+              console.error(err)
+              return
+          }
+      })
+  } catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
 
 async function db_query2(zip, year_x, year_y) {
 
@@ -109,7 +191,6 @@ async function db_query2(zip, year_x, year_y) {
 
 
 
-
 var server = http.createServer(function(req, res){
 
     // The following is mainly for navigation purposes
@@ -123,6 +204,10 @@ var server = http.createServer(function(req, res){
     
     if (temp.query=='1'){
         file = "./Query1.html";
+        if(temp.zip!== undefined){
+          db_query1(temp.zip, temp.year_x, temp.year_y);
+          file = 'Query1_chart.html';
+      }
     }
     if (temp.query=='2'){
         file = "./Query2.html";
