7750abde1f96ce529468e950d0f33e6ac90b8f70
jbarber877 <80484728+jbarber877@users.noreply.github.com>
2021-12-07 11:45:36 -0500

    Update q4.js
    Added sinkhole query

--- queries/q4.js
+++ queries/q4.js
@@ -11,3 +11,20 @@ let q4 = function(zip, from, to) {
 module.exports = {
     get: q4
 }
+
+
+ // The average sale prices and dates of sales where the property itself does not have a sinkhole, but another property in the neighborhood does
+        let new_query = "WITH Neighborhood (mu, cdate) AS (select avg(price), to_char(sale_date, 'YYYY-MM') \n";
+        new_query +=                                     " from Parcel NATURAL JOIN Land NATURAL JOIN Sale \n";
+        new_query +=                                     " where sinkhole_status is NULL \n";
+        new_query +=                                     ("AND SALE_DATE BETWEEN to_date('01/01/" + startYear + "', 'DD/MM/YYYY') and to_date('01/01/" + endYear + "', 'DD/MM/YYYY') \n");
+        new_query +=                                     " and neighborhood_code IN (select distinct neighborhood_code \n";
+        new_query +=                                                              " from parcel NATURAL JOIN Land \n";
+        new_query +=                                                              " where sinkhole_status is not NULL) \n";
+        new_query +=                                    " GROUP BY to_char(sale_date, 'YYYY-MM')), \n";  
+        // The average price in the zip code, regardless of sinkhole status
+        new_query += "ZipCode (nu, cdate) AS (select avg(price), to_char(sale_date, 'YYYY-MM') \n"; 
+        new_query +=                              " from Parcel NATURAL JOIN Land NATURAL JOIN Sale \n";
+        new_query +=                              " GROUP BY to_char(sale_date, 'YYYY-MM')) \n";
+        // The percent difference between sale prices in the affected neighborhood and the zipcode in general 
+        new_query += " select (1-((mu - nu)/mu)) as PercentDifference, cdate as Foo from Neighborhood NATURAL JOIN ZipCode where (1-((mu - nu)/mu)) < 2000000 order by cdate";
