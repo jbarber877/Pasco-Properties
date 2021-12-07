34d2e496c4409770b835ee946a78692749172bfa
jbarber877 <80484728+jbarber877@users.noreply.github.com>
2021-12-07 12:30:14 -0500

    Update server.js
    added q3

--- server.js
+++ server.js
@@ -188,7 +188,131 @@ async function db_query2(zip, year_x, year_y) {
   
 }
 
+async function db_query3(zip) {
+  let connection;
+  let chart_file_text;
+  try {
+         
+          connection = await oracledb.getConnection({user:"Barber.J", password: "RedCedar3", connectionString:"oracle.cise.ufl.edu/orcl" });
+          console.log("Connected to oracle database");
+  
+          //The average sale price in the zip code, regardless of homestead status
+          let query = "WITH AVG_ZIP_CODE_PRICE (average, cdate) AS (SELECT AVG(Price), to_char(sale_date, 'YYYY-MM')\n";
+          query +=                                              "FROM Parcel NATURAL JOIN Sale \n";
+          query +=                                              ("WHERE site_Zip = \n" + zip);
+          query +=                                              " GROUP BY to_char(sale_date, 'YYYY-MM')),\n ";
+          //The std deviation in the zip code where the parcel is homesteaded
+          query +=      "DEV_ZIP_CODE_HOME (stdHomestead, cdate) AS (SELECT STDDEV(Price), to_char(sale_date, 'YYYY-MM') \n";
+          query +=                                                   "FROM Parcel NATURAL JOIN Sale\n ";
+          query +=                                                   "WHERE Homestead LIKE '%Y%'\n ";
+          query +=                                                   ("AND site_Zip = " + zip);
+          query +=                                                   " GROUP BY to_char(sale_date, 'YYYY-MM')), \n";
+          //The std deviation in the zip code where the parcel IS NOT homesteaded
+          query +=     "DEV_ZIP_CODE_NO (stdNotHomestead, cdate)AS (SELECT STDDEV(Price), to_char(sale_date, 'YYYY-MM')\n ";
+          query +=                                                  "FROM Parcel NATURAL JOIN Sale\n "
+          query +=                                                  "WHERE Homestead NOT LIKE '%Y%' \n"
+          query +=                                                  ("AND site_Zip = \n" + zip);
+          query +=                                                   " GROUP BY to_char(sale_date, 'YYYY-MM'))\n";
+          // The actual query itself                                   
+          query += "SELECT stdHomestead, stdNotHomestead, average, cdate\n";
+          query += "FROM AVG_ZIP_CODE_PRICE NATURAL JOIN DEV_ZIP_CODE_HOME NATURAL JOIN DEV_ZIP_CODE_NO\n";
+          query += "where stdNotHomestead < 1000000 \n";
+          query += "ORDER BY cdate";
 
+    // Write everything to an html file. Sort the query data into strings 
+    let yAxis = "";
+    let stdHomestead = "";
+    let stdNot = "";
+
+    let result = await connection.execute(query, []);
+    //console.log('result = ' + result);
+
+    for (let i = 0; i < result.rows.length; i++) { 
+        stdHomestead += (result.rows[i][0]);
+        stdNot += (result.rows[i][1]);
+        // The average may be useful as a baseline, but is not displayed on the graph
+        //avg += (result.rows[i][2]); 
+        yAxis += ("'" + result.rows[i][3] + "'");
+
+        if (i+1 < result.rows.length){
+            stdHomestead += ",";
+            stdNot += ",";
+            yAxis += ",";
+        }
+    }
+
+      // pass info from database to chart
+      chart_file_text = `<!DOCTYPE html>
+      <html lang="en">
+          <head>
+          </head>
+          <body>
+              <h1>Query Results</h1>
+              <h2 id="return">return</h2>
+              <div >
+                  <canvas id="myChart"></canvas>
+              </div>
+              <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+              <script >
+                  document.getElementById("return").onclick = () => {
+                      location.search = '';
+                  }
+                  const labels = [` + yAxis + `];
+          const data = {
+            labels: labels,
+            datasets: [{
+              label: 'Sale Prices of Homesteaded Properties in ` + zip + `',
+              backgroundColor: 'rgb(255, 99, 132)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: [` + stdHomestead + `]
+          },
+          {
+            label: 'Sale Prices of Non-homesteaded Properties in ` + zip + `',
+            backgroundColor: 'rgb(0, 99, 132)',
+            borderColor: 'rgb(0, 99, 132)',
+            data: [` + stdNot + `]
+        }]
+          };
+          const config = {
+            type: 'line',
+            data: data,
+            options: {}
+          };
+            // === include 'setup' then 'config' above ===
+          
+            const myChart = new Chart(
+              document.getElementById('myChart'),
+              config
+            );
+              </script>
+          </body>
+      </html>` 
+      fs.writeFile('Query3_chart.html', chart_file_text, err => {
+          if (err){
+              console.error(err)
+              return
+          }
+      })
+  } 
+  
+  
+  
+  
+  
+  
+  
+  catch (err) {
+    console.error(err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+}
 
 
 var server = http.createServer(function(req, res){
